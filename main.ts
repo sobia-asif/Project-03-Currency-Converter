3b8e9051f8ebefce28331b2757db7c684a70855a
sobia-asif <sobiaarf1728@gmail.com>
2024-05-01 13:45:29 -0700

    push my currency converter to github

---
+++ main.ts
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+
+const currency: any = {
+    USD: 1, // base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+
+}
+
+let user_answer= await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: 'Enter from Currency',
+            type: 'list',
+            choices: ["USD","EUR", "GBP","INR","PKR"]
+        },
+
+        {
+            name: 'to',
+            message: 'Enter to Currency',
+            type: 'list',
+            choices: ["USD","EUR", "GBP","INR","PKR"]
+        },
+
+        {
+            name: 'amount',
+            message: 'Enter your Amount',
+            type: 'number',
+            
+        }
+    ]
+)
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount /fromAmount // usd base currency//4
+                15000/280
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+
+
+
+
+
+
+
+
+
+
+
